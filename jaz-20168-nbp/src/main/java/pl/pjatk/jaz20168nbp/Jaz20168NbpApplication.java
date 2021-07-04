65cb2bbd49452b6d06301482942f83c65d310768
s20168 <s201682@pjwstk.edu.pl>
2021-07-04 20:46:02 +0200

    Added missing files from wrong commit earlier

---
+++ jaz-20168-nbp/src/main/java/pl/pjatk/jaz20168nbp/Jaz20168NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz20168nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jaz20168NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jaz20168NbpApplication.class, args);
+	}
+
+}
---
+++ jaz-20168-nbp/src/test/java/pl/pjatk/jaz20168nbp/Jaz20168NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jaz20168nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jaz20168NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
